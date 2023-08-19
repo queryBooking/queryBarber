05dbedaabba9673d3794cf32b662ce9cae989457
queryBooking <137189032+queryBooking@users.noreply.github.com>
2023-08-19 21:41:04 +0700

    3 files modified, 4 added, 2 deleted

---
+++ ggDriveAPI.js
@@ -0,0 +1,3 @@
+import { google } from 'googleapis';
+const Client_id =
+  '367165157540-hmljmvrmjmsr81ap3aeaaotvnk2uqesr.apps.googleusercontent.com';
--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-import '~/css/style.css';
-
-success();
-
-function success() {
-  const latitude = 17.2194059;
-  const longitude = 100.1993588;
-  const latLong = [latitude, longitude];
-
-  const map = L.map('map').setView(latLong, 13);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 25,
-    attribution: '© OpenStreetMap',
-  }).addTo(map);
-
-  const marker = L.marker(latLong).addTo(map);
-  const mapCenter = map.getCenter();
-  marker.setLatLng(mapCenter);
-
-  const circle = L.circle(latLong, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500,
-  }).addTo(map);
-}
+import './style.css';
+import './ggDriveAPI.js';
+import './map.js';
