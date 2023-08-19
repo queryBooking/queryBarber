86fea062c7b76820f8f47a2229bc1492da580aeb
queryBooking <137189032+queryBooking@users.noreply.github.com>
2023-08-19 22:31:33 +0700

    2 files modified

--- ggDriveAPI.js
+++ ggDriveAPI.js
@@ -1,9 +1,19 @@
 import { google } from 'googleapis';
-import { OAuth2Client } from 'oauth2-client-js';
+import { https } from 'https';
 const CLIENT_ID =
   '367165157540-hmljmvrmjmsr81ap3aeaaotvnk2uqesr.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
 const REFRESH_TOKEN =
   '1//048qV1Zb2gmN7CgYIARAAGAQSNwF-L9IrqWB_eZPQuyOdoy9hpXgyfmTO0COPHJby1r2d9dMhXGWV9SrxddNprHYHtZbQvix67eA';
-const oauth2 = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+
+// Set the refresh token (optional if you already have it)
+oauth2Client.setCredentials({
+  refresh_token: REFRESH_TOKEN,
+});
