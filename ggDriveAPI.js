9de33089b29b5de94946f82514e24ec81677e229
queryBooking <137189032+queryBooking@users.noreply.github.com>
2023-08-20 11:56:29 +0700

    1 file modified

--- ggDriveAPI.js
+++ ggDriveAPI.js
@@ -1,5 +1,5 @@
-/*import { google } from 'googleapis';
-import { OAuth2Client } from 'google-auth-library';
+import { google } from 'googleapis';
+import ClientOAuth2 from 'client-oauth2';
 
 const CLIENT_ID =
   '367165157540-hmljmvrmjmsr81ap3aeaaotvnk2uqesr.apps.googleusercontent.com';
@@ -8,5 +8,4 @@ const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
 const REFRESH_TOKEN =
   '1//048qV1Zb2gmN7CgYIARAAGAQSNwF-L9IrqWB_eZPQuyOdoy9hpXgyfmTO0COPHJby1r2d9dMhXGWV9SrxddNprHYHtZbQvix67eA';
 
-  const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL );
-  */
+//const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL );
